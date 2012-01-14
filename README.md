298caa51a76aaafc90f7bfec76c860134862b5ab
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-01-14 18:44:57 -0500

    edding the README to make it more easy to use

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This addons originaly born while I was working on [Efecto Mariposa](http://patri
 The main goal of this addon it´s to make super easy to use and edit shaders on-the-fly for adding them on your proyects.
 
 ## ofxFXObject ##
-It´s the parent class of all the other effect. If you want to make a new filter it´s better if you start looking at the ofxFXObject.h in order to see how it works. 
+It´s the parent class of all the other effect. If you want to make a new filter it´s better if you start looking at the ```ofxFXObject.h``` in order to see how it works. 
 
 It have a simple structre:
 
@@ -18,7 +18,7 @@ It have a simple structre:
 
 3. injectShader(): here is where the shaders are loaded. The good stuff cames when you load them on the fly through a string using something like this:
 
-```
+```c++
 ofxFXObject fxObject = ofxFXObject();
 
 fxObject.allocate(800,600); // At this point it will load a default timer shader
@@ -47,25 +47,28 @@ fxObject.injectShader("#version 120\n\
                     }"); 
 ```
 
-### Copy, Pase & Inject ###
-http://www.iquilezles.org/apps/shadertoy/
-http://mrdoob.com/projects/glsl_sandbox/
+3. begin(int texN) & end (int texN): remember nTextures variable? you can passthrough information to it by using this end() and begin() and giving the argument the number of texture you want to open. This texture can be access from the shader by the ```uniform sample2DRect tex0```` or ```tex1``` or ```tex2``` and so on. Also you can access to the previus FBO of the ping pong by ```calling uniform sample2DRect backbuffer```. 
 
-### Portability ###
-Each ofxFX class have a fragment, vertex o geometry Shader embebed on it´s code. So you don´t need to add any kind of .frag , .vert or .geom on your /data forlder
+4. update(): this is the core of the class, where the magic happends. If you check  for the ```ofxFXObject::update()``` you will see how the tex´s, the backbuffer and other default uniforms variables (time, mouse, resolution) are loaded.
 
-### Coherence ###
-The implementation of each ofxFX class it´s consistent with each other. All of them are extensions of ofxFXObject
+5. draw(): after all you defenetly want to look at it.
 
-* All of them need to be allocated first in order to 
-```fx.allocate(width,height);```
 
-* All of them have a ofxFbo style. So you can draw or render directly inside of them. Allowing you to easyly interact with them
-```
-fx.begin();
-ofEllipse(50,50,10,10);
-fx.end();
-```
+Also in this class you can found some handy funtion as:
+
+* renderFrame(width,height) makes a frame where the textures could be draw;
+* initFbo(ofxFbo, width, height, internalFormat ): it takes the work of allocating and cleaning an FBO.
+* ofxSwapBuffer: this is actualy a class for making easy dealing with ping-pongs.
+
+Moust of the subClass that inherit from ofxFXObject that are part of this addon are ones that in some point breaks this logic and structure and use just some elements and re-define new ones.
+
+### Copy, Inject & Share ###
+The fun part start here. Go to [Ricardo Caballero´s webGL Sandbox](http://mrdoob.com/projects/glsl_sandbox/) or [ShaderToy by Inigo Quilez](http://www.iquilezles.org/apps/shadertoy/) found an efect that you like. Copy the code. Inject it and compile.
+Explore new changes and share them.
+
+Note: it´s important to makes all the changes for openGL version 1.2 using ARB Rect textures
+
+## Examples ##
 
 Watch some of this classes in action [HERE](http://www.patriciogonzalezvivo.com/blog/?p=488)
 
--- example-ofxFXObject/src/testApp.cpp
+++ example-ofxFXObject/src/testApp.cpp
@@ -218,72 +218,71 @@ void testApp::setup(){
     uniform float time;\
     uniform vec2 mouse;\
     uniform vec2 resolution;\
-    uniform sampler2D backbuffer;\
+    uniform sampler2DRect backbuffer;\
     \
-    float getSpring(float r, vec2 pos, float power){\
-        return ( texture2D(backbuffer, pos).r - r) * power;\
+    float getSpring(float r, vec2 stPos, float power){\
+        return ( texture2DRect(backbuffer, stPos).r - r) * power;\
     }\
     \
     void main(){\
-        vec2 pos = gl_FragCoord.xy / resolution;\
-        vec2 pixel = 1. / resolution;\
+        vec2 st = gl_TexCoord[0].st;\
         float aspect = resolution.x / resolution.y;\
         \
-        vec4 texel_prev = texture2D(backbuffer, pos);\
+        vec4 texel_prev = texture2DRect(backbuffer, st);\
         \
         float r_prev = texel_prev.r;\
         float power = .5;\
         \
         float vel = texel_prev.a - 0.5;\
-        vel += getSpring(r_prev, pos + pixel * vec2(2, 3), 0.0022411859348636983 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(1, 3), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(0, 3), 0.005681818181818182 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-1, 3), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-2, 3), 0.0022411859348636983 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(3, 2), 0.0022411859348636983 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(2, 2), 0.0066566640639421 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(1, 2), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(0, 2), 0.011363636363636364 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-1, 2), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-2, 2), 0.0066566640639421 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-3, 2), 0.0022411859348636983 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(3, 1), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(2, 1), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(1, 1), 0.014691968395607415 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(0, 1), 0.017045454545454544 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-1, 1), 0.014691968395607415 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-2, 1), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-3, 1), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(3, 0), 0.005681818181818182 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(2, 0), 0.011363636363636364 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(1, 0), 0.017045454545454544 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-1, 0), 0.017045454545454544 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-2, 0), 0.011363636363636364 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-3, 0), 0.005681818181818182 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(3, -1), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(2, -1), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(1, -1), 0.014691968395607415 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(0, -1), 0.017045454545454544 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-1, -1), 0.014691968395607415 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-2, -1), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-3, -1), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(3, -2), 0.0022411859348636983 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(2, -2), 0.0066566640639421 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(1, -2), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(0, -2), 0.011363636363636364 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-1, -2), 0.010022341036933013 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-2, -2), 0.0066566640639421 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-3, -2), 0.0022411859348636983 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(2, -3), 0.0022411859348636983 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(1, -3), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(0, -3), 0.005681818181818182 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-1, -3), 0.004759786021770571 * power);\
-        vel += getSpring(r_prev, pos + pixel * vec2(-2, -3), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(2, 3), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(1, 3), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(0, 3), 0.005681818181818182 * power);\
+        vel += getSpring(r_prev, st + vec2(-1, 3), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(-2, 3), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(3, 2), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(2, 2), 0.0066566640639421 * power);\
+        vel += getSpring(r_prev, st + vec2(1, 2), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(0, 2), 0.011363636363636364 * power);\
+        vel += getSpring(r_prev, st + vec2(-1, 2), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(-2, 2), 0.0066566640639421 * power);\
+        vel += getSpring(r_prev, st + vec2(-3, 2), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(3, 1), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(2, 1), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(1, 1), 0.014691968395607415 * power);\
+        vel += getSpring(r_prev, st + vec2(0, 1), 0.017045454545454544 * power);\
+        vel += getSpring(r_prev, st + vec2(-1, 1), 0.014691968395607415 * power);\
+        vel += getSpring(r_prev, st + vec2(-2, 1), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(-3, 1), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(3, 0), 0.005681818181818182 * power);\
+        vel += getSpring(r_prev, st + vec2(2, 0), 0.011363636363636364 * power);\
+        vel += getSpring(r_prev, st + vec2(1, 0), 0.017045454545454544 * power);\
+        vel += getSpring(r_prev, st + vec2(-1, 0), 0.017045454545454544 * power);\
+        vel += getSpring(r_prev, st + vec2(-2, 0), 0.011363636363636364 * power);\
+        vel += getSpring(r_prev, st + vec2(-3, 0), 0.005681818181818182 * power);\
+        vel += getSpring(r_prev, st + vec2(3, -1), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(2, -1), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(1, -1), 0.014691968395607415 * power);\
+        vel += getSpring(r_prev, st + vec2(0, -1), 0.017045454545454544 * power);\
+        vel += getSpring(r_prev, st + vec2(-1, -1), 0.014691968395607415 * power);\
+        vel += getSpring(r_prev, st + vec2(-2, -1), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(-3, -1), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(3, -2), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(2, -2), 0.0066566640639421 * power);\
+        vel += getSpring(r_prev, st + vec2(1, -2), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(0, -2), 0.011363636363636364 * power);\
+        vel += getSpring(r_prev, st + vec2(-1, -2), 0.010022341036933013 * power);\
+        vel += getSpring(r_prev, st + vec2(-2, -2), 0.0066566640639421 * power);\
+        vel += getSpring(r_prev, st + vec2(-3, -2), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(2, -3), 0.0022411859348636983 * power);\
+        vel += getSpring(r_prev, st + vec2(1, -3), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(0, -3), 0.005681818181818182 * power);\
+        vel += getSpring(r_prev, st + vec2(-1, -3), 0.004759786021770571 * power);\
+        vel += getSpring(r_prev, st + vec2(-2, -3), 0.0022411859348636983 * power);\
         \
         vel += (.25 - r_prev) * .025 * power;\
         \
         vec2 m = mouse / resolution;\
-        vel += max(0., .1 * (1. - (length((pos - m) * vec2(aspect, 1.)) * 15.)));\
+        vel += max(0., .1 * (1. - (length((st - m) * vec2(aspect, 1.)) * 15.)));\
         \
         gl_FragColor = vec4(texel_prev.rgb + vel, vel * .98 + .5);\
     }";
