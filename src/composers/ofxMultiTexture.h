1d406597d3b9f14061e5fb2063d812d478548434
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-03-06 22:16:27 -0300

    LUT and multitexture

--- src/composers/ofxMultiTexture.h
+++ src/composers/ofxMultiTexture.h
@@ -1,14 +1,58 @@
-//
-//  ofxMultiTexture.h
-//  InstagramLikeImageFilters
-//
-//  Created by Patricio González Vivo on 3/1/12.
-//  Copyright (c) 2012 PatricioGonzalezVivo.com. All rights reserved.
-//
+/*
+ *  ofxMask.h
+ *
+ *  Created by Patricio González Vivo on 10/1/11.
+ *  Copyright 2011 http://PatricioGonzalezVivo.com All rights reserved.
+ *
+ *  Based on ofxAlphaMaskShader
+ *  Created by James George, http://www.jamesgeorge.org
+ *  in collaboration with FlightPhase http://www.flightphase.com
+ *
+ *  ******************************************************************
+ *
+ *  tex0 -> multi texture map
+ *  tex1 -> RED CHANNEL
+ *  tex2 -> GREEN CHANNEL
+ *  tex3 -> BLUE CHANNEL
+ *
+ */
 
-#ifndef InstagramLikeImageFilters_ofxMultiTexture_h
-#define InstagramLikeImageFilters_ofxMultiTexture_h
+#ifndef OFXMULTITEXTURE
+#define OFXMULTITEXTURE
 
+#include "ofMain.h"
+#include "ofxFXObject.h"
 
+class ofxMultiTexture : public ofxFXObject {	
+public:
+    ofxMultiTexture(){
+        passes = 1;
+        internalFormat = GL_RGBA;
+        
+        fragmentShader = STRINGIFY(
+                                   uniform sampler2DRect tex0;
+                                   uniform sampler2DRect tex1;
+                                   uniform sampler2DRect tex2;
+                                   uniform sampler2DRect tex3;
+                                   
+                                   void main (void){
+                                       vec2 pos = gl_TexCoord[0].st;
+                                       
+                                       vec4 mask = texture2DRect(tex0, pos);
+                                       
+                                       vec4 rTxt = texture2DRect(tex1, pos);
+                                       vec4 gTxt = texture2DRect(tex2, pos);
+                                       vec4 bTxt = texture2DRect(tex3, pos);
+                                       
+                                       vec4 color = vec4(0.0,0.0,0.0,1.0);
+                                       color = mix(color, rTxt, mask.r );
+                                       color = mix(color, gTxt, mask.g );
+                                       color = mix(color, bTxt, mask.b );
+                                       
+                                       gl_FragColor = color;
+                                   }
+                                   );
+    }
+};
 
 #endif
--- src/filters/ofxLUT.h
+++ src/filters/ofxLUT.h
@@ -1,14 +1,83 @@
-//
-//  ofxLUT.h
-//  InstagramLikeImageFilters
-//
-//  Created by Patricio González Vivo on 3/1/12.
-//  Copyright (c) 2012 PatricioGonzalezVivo.com. All rights reserved.
-//
+/*
+ *  ofxMask.h
+ *
+ *  Created by Patricio González Vivo on 10/1/11.
+ *  Copyright 2011 http://PatricioGonzalezVivo.com All rights reserved.
+ *
+ *  ******************************************************************
+ *
+ *  tex0 -> Image
+ *  tex1 -> LUT
+ *
+ */
 
-#ifndef InstagramLikeImageFilters_ofxLUT_h
-#define InstagramLikeImageFilters_ofxLUT_h
+#ifndef OFXLUT
+#define OFXLUT
 
+#include "ofMain.h"
+#include "ofxFXObject.h"
 
+class ofxLUT : public ofxFXObject {	
+public:
+    ofxLUT(){
+        passes = 1;
+        internalFormat = GL_RGB32F;
+        
+        fragmentShader = STRINGIFY(
+                                   uniform sampler2DRect tex0;
+                                   uniform sampler2DRect tex1;
+                                   
+                                   float size = 32.0;
+                                   
+                                   void main(void){
+                                       vec2 st = gl_TexCoord[0].st;
+                                       
+                                       vec4 srcColor = texture2DRect(tex0, st);
+                                       
+                                       float x = srcColor.r * (size-1.0);
+                                       float y = srcColor.g * (size-1.0);
+                                       float z = srcColor.b * (size-1.0);
+                                       
+                                       vec3 color = texture2DRect(tex1, vec2(floor(x)+1.0 +(floor(y)+1.0)*size,
+                                                                             floor(z)+1.0)).rgb;
+                                       
+                                       gl_FragColor = vec4( color , 1.0);
+                                   }
+                                   );
+    }
+    
+    bool loadLUT(string lutFile){
+        int mapSize = 32;
+        
+        ofBuffer buffer = ofBufferFromFile(lutFile);
+        
+        float * pixels = new float[mapSize * mapSize * mapSize * 3];
+        
+        textures[1].getTextureReference().allocate(mapSize * mapSize, mapSize,GL_RGB32F);
+        
+        ofDirectory dir;
+        
+        for(int z=0; z<mapSize ; z++){
+            for(int y=0; y<mapSize; y++){
+                for(int x=0; x<mapSize; x++){
+                    string content = buffer.getNextLine();
+                    int pos = x + y*mapSize + z*mapSize*mapSize;
+                    
+                    vector <string> splitString = ofSplitString(content, " ", true, true);
+                    
+                    if (splitString.size() >=3) {
+                        pixels[pos*3 + 0] = ofToFloat(splitString[0]);
+                        pixels[pos*3 + 1] = ofToFloat(splitString[1]);
+                        pixels[pos*3 + 2] = ofToFloat(splitString[2]);
+                    }
+                }
+            }
+        }    
+        
+        textures[1].getTextureReference().loadData( pixels, mapSize * mapSize, mapSize, GL_RGB);
 
+        return true;        
+    }
+    
+};
 #endif
