9fd5a6cb0d3550d27ce1f703e9163f736598a19b
Oliver Uvman <oliver.uvman@gmail.com>
2014-03-25 01:18:06 +0100

    ofBlur.h with backbuffer and injectUniforms

--- src/filters/ofxBlur.h
+++ src/filters/ofxBlur.h
@@ -50,84 +50,60 @@
 #include "ofxFXObject.h"
 
 class ofxBlur : public ofxFXObject {
-public:
-    ofxBlur(){
-        passes = 3;
-        internalFormat = GL_RGBA;
-        
-        // Fade constant
-        fade = 0.03f;
-        
-        // In this example the tex0 it's more like a backbuffer 
-        fragmentShader = STRINGIFY(
-                                   uniform sampler2DRect tex0;
-                                   uniform float fade;
-                                   
-                                   float kernel[9];
-                                   vec2 offset[9];
-                                   
-                                   void main(void){
-                                       vec2  st = gl_TexCoord[0].st;
-                                       vec4 sum = vec4(0.0);
-                                       
-                                       offset[0] = vec2(-1.0, -1.0);
-                                       offset[1] = vec2(0.0, -1.0);
-                                       offset[2] = vec2(1.0, -1.0);
-                                       
-                                       offset[3] = vec2(-1.0, 0.0);
-                                       offset[4] = vec2(0.0, 0.0);
-                                       offset[5] = vec2(1.0, 0.0);
-                                       
-                                       offset[6] = vec2(-1.0, 1.0);
-                                       offset[7] = vec2(0.0, 1.0);
-                                       offset[8] = vec2(1.0, 1.0);
-                                       
-                                       kernel[0] = 1.0/16.0;   kernel[1] = 2.0/16.0;   kernel[2] = 1.0/16.0;
-                                       kernel[3] = 2.0/16.0;   kernel[4] = 4.0/16.0;   kernel[5] = 2.0/16.0;
-                                       kernel[6] = 1.0/16.0;   kernel[7] = 2.0/16.0;   kernel[8] = 1.0/16.0;
-                                       
-                                       int i = 0;
-                                       for (i = 0; i < 9; i++){
-                                           vec4 tmp = texture2DRect(tex0, st + offset[i]);
-                                           sum += tmp * kernel[i];
-                                       }
-                                       
-                                       vec4 previousValue = texture2DRect(tex0, st);
-                                       gl_FragColor = (1.0 - fade) * previousValue +  fade * vec4(sum.rgb, previousValue.a);
-                                   }
-                                   );
-         
-    };
-    
-	void setFade(float _fade) { fade = _fade;};
-    
-	void update(){
-        ofPushStyle();
-        
-        ofSetColor(255);
-        pingPong.src->begin();
-        ofClear(0,0);
-        textures[0].draw(0,0);
-        pingPong.src->end();
-        
-        for(int i = 0; i < passes; i++) {
-            pingPong.dst->begin();
-            ofClear(0,0);
-            shader.begin();
-            // In this example the tex0 it's more like a backbuffer 
-            shader.setUniformTexture("tex0", pingPong.src->getTextureReference(), 0 );
+    public:
+        float fade;
+        void setFade(float _fade) { fade = _fade;};
+
+        ofxBlur(){
+            passes = 3;
+            internalFormat = GL_RGBA;
+
+            // Fade constant
+            fade = 0.03f;
+
+            // Since we want to run several passes and we have backbuffer declared,
+            // it will contain the first frame we want to act on during the first
+            // pass, and after that it will contain the result of each previous pass.
+            fragmentShader = STRINGIFY(
+                    uniform sampler2DRect backbuffer;
+                    uniform float fade;
+
+                    float kernel[9];
+                    vec2 offset[9];
+
+                    void main(void){
+                        vec2  st = gl_TexCoord[0].st;
+                        vec4 sum = vec4(0.0);
+
+                        offset[0] = vec2(-1.0, -1.0);
+                        offset[1] = vec2(0.0, -1.0);
+                        offset[2] = vec2(1.0, -1.0);
+
+                        offset[3] = vec2(-1.0, 0.0);
+                        offset[4] = vec2(0.0, 0.0);
+                        offset[5] = vec2(1.0, 0.0);
+
+                        offset[6] = vec2(-1.0, 1.0);
+                        offset[7] = vec2(0.0, 1.0);
+                        offset[8] = vec2(1.0, 1.0);
+
+                        kernel[0] = 1.0/16.0;   kernel[1] = 2.0/16.0;   kernel[2] = 1.0/16.0;
+                        kernel[3] = 2.0/16.0;   kernel[4] = 4.0/16.0;   kernel[5] = 2.0/16.0;
+                        kernel[6] = 1.0/16.0;   kernel[7] = 2.0/16.0;   kernel[8] = 1.0/16.0;
+
+                        int i = 0;
+                        for (i = 0; i < 9; i++){
+                            vec4 tmp = texture2DRect(backbuffer, st + offset[i]);
+                            sum += tmp * kernel[i];
+                        }
+
+                        vec4 previousValue = texture2DRect(backbuffer, st);
+                        gl_FragColor = (1.0 - fade) * previousValue +  fade * vec4(sum.rgb, previousValue.a);
+                    });
+        };
+
+    protected:
+        void injectUniforms() {
             shader.setUniform1f("fade", fade);
-            renderFrame();
-            shader.end();
-            pingPong.dst->end();
-            
-            pingPong.swap();
         }
-        
-        pingPong.swap();
-        
-        ofPopStyle();
-	}
-	
-    float       fade;
 };
