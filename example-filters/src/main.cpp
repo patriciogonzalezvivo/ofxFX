4622c77c2ca4dfad97df457308bd45ef3698576e
Mikhail S. Kataev <lpestlname@gmail.com>
2019-03-05 16:32:17 +0300

    Update example-filters

--- example-filters/src/main.cpp
+++ example-filters/src/main.cpp
@@ -1,15 +1,11 @@
 #pragma once
 
 #include "ofMain.h"
-
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
-
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
